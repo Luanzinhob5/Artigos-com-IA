0e6ad2938160bbdb25a73941acbb24e41d4b38ee
Luan <83055293+Luanzinhob5@users.noreply.github.com>
2025-08-07 16:08:40 -0300

    first commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from crew import CriadorBlog
+from dotenv import load_dotenv
+import os
+
+#Carrega a API_KEY da openai no arquivo .env
+load_dotenv()
+os.environ["OPENAI_API_KEY"]
+
+
+CriadorBlog().crew().kickoff()
